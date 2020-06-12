650d7b6d0735483c3875f5a8b1813ba7740b064e
Miika Myötänen <miika.myotanen@hotmail.com>
2020-06-12 19:04:09 +0300

    Update Kolikot.java

--- Kolikot.java
+++ Kolikot.java
@@ -1,3 +1,14 @@
+/* Sinulla on n kolikkoa, joista jokaisella on tietty arvo. Montako eri rahamäärää voit muodostaa kolikoiden avulla?
+
+Esimerkiksi jos kolikoita on 3 ja niiden arvot ovat 1, 1, ja 4, voit muodostaa 5 eri rahamäärää: 1, 2, 4, 5 ja 6.
+
+Tee luokka Kolikot, jossa on seuraavat metodit:
+
+int laske(int[] t): ilmoittaa, montako eri rahamäärää voi muodostaa
+
+*/
+
+
 import java.util.*;
 
 public class Kolikot {  
