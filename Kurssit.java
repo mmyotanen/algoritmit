d2501e823813b1adccac660957c976ef83ce34b3
Miika Myötänen <miika.myotanen@hotmail.com>
2020-06-12 19:29:49 +0300

    Add files via upload

---
+++ Kurssit.java
@@ -0,0 +1,130 @@
+import java.util.*;
+
+public class Kurssit {
+    private class Node {
+    private String label;
+
+    public Node(String label) {
+      this.label = label;
+    }
+    }
+    private Map<String, Node> nodes = new HashMap<>();
+    private Map<Node, List<Node>> adjacencyList = new HashMap<>();
+    private String root = "a";
+    
+    public Kurssit() {
+        
+    }
+    
+    public void lisaaKurssi(String s) {
+        if (root.equals("a")) {
+            root = s;
+        }
+        Node node = new Node(s);
+        nodes.putIfAbsent(s, node);
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+ 
+    public void lisaaVaatimus(String a, String b) {
+        Node fromNode = nodes.get(a);
+    if (fromNode == null)
+      throw new IllegalArgumentException();
+
+    Node toNode = nodes.get(b);
+    if (toNode == null)
+      throw new IllegalArgumentException();
+
+    adjacencyList.get(fromNode).add(toNode);
+    }
+    
+    public ArrayList<String> muodosta() {
+        if (hasCycle()) return null;
+        traverseDepthFirstRec(root);
+        ArrayList<String> tulostus = topologicalSort();
+        return tulostus;
+        
+    }
+    public void traverseDepthFirstRec(String root) {
+    Node node = nodes.get(root);
+    if (node == null)
+      return;
+
+    traverseDepthFirstRec(node, new HashSet<>());
+  }
+
+  private void traverseDepthFirstRec(Node root, Set<Node> visited) {
+    //System.out.println(root);
+    visited.add(root);
+
+    for (Node node : adjacencyList.get(root))
+      if (!visited.contains(node))
+        traverseDepthFirstRec(node, visited);
+  }
+  public ArrayList<String> topologicalSort() {
+    Stack<Node> stack = new Stack<>();
+    Set<Node> visited = new HashSet<>();
+
+    for (Node node : nodes.values())
+      topologicalSort(node, visited, stack);
+
+    ArrayList<String> sorted = new ArrayList<>();
+    while (!stack.empty())
+      sorted.add(stack.pop().label);
+
+    return sorted;
+  }
+
+  private void topologicalSort(
+      Node node, Set<Node> visited, Stack<Node> stack) {
+    if (visited.contains(node))
+      return;
+
+    visited.add(node);
+
+    for (Node neighbour : adjacencyList.get(node))
+      topologicalSort(neighbour, visited, stack);
+
+    stack.push(node);
+  }
+  public boolean hasCycle() {
+    Set<Node> all = new HashSet<>();
+    all.addAll(nodes.values());
+
+    Set<Node> visiting = new HashSet<>();
+    Set<Node> visited = new HashSet<>();
+
+    while (!all.isEmpty()) {
+      Node current = all.iterator().next();
+      if (hasCycle(current, all, visiting, visited))
+        return true;
+    }
+
+    return false;
+  }
+
+  private boolean hasCycle(Node node, Set<Node> all,
+                 Set<Node> visiting, Set<Node> visited) {
+    all.remove(node);
+    visiting.add(node);
+
+    for (Node neighbour : adjacencyList.get(node)) {
+      if (visited.contains(neighbour))
+        continue;
+
+      if (visiting.contains(neighbour))
+        return true;
+
+      if (hasCycle(neighbour, all, visiting, visited))
+        return true;
+    }
+
+    visiting.remove(node);
+    visited.add(node);
+
+    return false;
+  }
+    
+    
+ 
+}
+
---
+++ Reitit.java
@@ -0,0 +1,67 @@
+import java.util.*;
+ 
+public class Reitit {
+ 
+    private int n;
+    private ArrayList<Integer> order;
+    private ArrayList<Integer>[] vertices;
+    private ArrayList<Integer>[] parents;
+    private boolean[] kayty;
+    private long[] polut;
+ 
+    public Reitit(int n) {
+        this.n = n;
+        this.order = new ArrayList<>();
+        this.vertices = new ArrayList[this.n+1];
+        this.parents = new ArrayList[this.n+1];        
+        for (int i = 1; i <= n; i++) {
+            vertices[i] = new ArrayList<>();
+            parents[i] = new ArrayList<>();
+        }
+        this.kayty = new boolean[n+1];
+        for (int i = 0; i < n + 1; i++) {
+            this.kayty[i] = false;
+        }
+        this.polut = new long[n+1];
+        this.polut[1] = 1;
+    }
+ 
+    public void lisaaKaari(int a, int b) {
+        this.vertices[a].add(b);
+        parents[b].add(a);
+    }
+ 
+    public long laske() {
+        
+        topoSort(1);
+        
+        Collections.reverse(order);
+        
+        for(int i = 0; i < n; i++) {
+            if (i == order.size()) break;
+            for(int child : parents[order.get(i)]) {
+                polut[order.get(i)] += polut[child];
+            }
+        }   
+        return polut[n];
+    }
+ 
+    public void topoSort(int solmu) {
+ 
+        if (this.vertices[solmu].isEmpty()) {
+            this.order.add(solmu);
+            this.kayty[solmu] = true;
+            return;
+        }
+        for (int node : this.vertices[solmu]) {
+            if (this.kayty[node]) {
+                continue;
+            }
+            topoSort(node);
+        }
+        this.kayty[solmu] = true;
+        this.order.add(solmu);
+        
+        
+    }
+}
