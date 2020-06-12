241b46cf62719ffd2f5fbb8b599144a41b840a5d
Miika Myötänen <miika.myotanen@hotmail.com>
2020-06-12 18:26:24 +0300

    Update Numerot.java

--- Numerot.java
+++ Numerot.java
@@ -2,7 +2,7 @@
 import java.util.stream.IntStream;
 
 
-
+// Laskee annetun positiivisen kokonaisluvun numeroiden summa. Esimerkiksi luvun 4075 numeroiden summa on 4 + 0 + 7 + 5 = 16.
 public class Numerot {
     int summa(int x) {
         int[] intTab = String.valueOf(x).chars().map(Character::getNumericValue).toArray();
