2e71e02f166a021e526a34d7e1f2ce6c356ce7bb
Miika Myötänen <miika.myotanen@hotmail.com>
2020-06-12 19:36:19 +0300

    Update Maksimivirtaus.java

--- Maksimivirtaus.java
+++ Maksimivirtaus.java
@@ -1,126 +1,59 @@
+/* Annettuna on suunnattu painotettu verkko, jossa on n solmua, jotka on numeroitu 1, 2, ..., n. 
+Tehtäväsi on määrittää maksimivirtaus solmusta 1 solmuun n.
+
+Tee luokka Maksimivirtaus, jossa on seuraavat metodit:
+
+Maksimivirtaus(int n): solmujen määrä annetaan konstuktorissa
+void lisaaKaari(int a, int b, int p): lisää verkkoon solmusta a solmuun b kaaren, jonka paino on p
+int laske(): ilmoittaa maksimivirtauksen suuruuden
+
+
+*/
+
+
 import java.util.*;
-
+ 
 public class Maksimivirtaus {     
-    public static class Edge {
-    public Edge residual;
-    public int to, capacity;
-    public final int originalCapacity;
-    public Edge(int to, int capacity) {
-      this.to = to; 
-      this.capacity = capacity;
-      this.originalCapacity = capacity;
-    }
-  }
-    private int n, source, sink;
-
-  // Internal
-  private int visitedToken = 1;
-  private int[] visited;
-  private boolean solved;
-
-  // Outputs
-  private int maxFlow;
-  private boolean[] minCut;
-  private List<List<Edge>> graph;
+    int n;
+    int[][] paino;
+    int tulos;
+    
     public Maksimivirtaus(int n) {
-        this.n = n+1;
-    initializeGraph();
-    this.source = 1;
-    this.sink = n;
+        this.n = n;
+        paino = new int[n+1][n+1];
     }
-    
-
+ 
     public void lisaaKaari(int a, int b, int p) {
-        Edge e1 = new Edge(b, p);
-    Edge e2 = new Edge(a, 0);
-    e1.residual = e2;
-    e2.residual = e1;
-    graph.get(a).add(e1);
-    graph.get(b).add(e2);
+        paino[a][b] += p;        
     }
     
+    int[] tila;
+    
+    int haku(int s, int u) {
+        if (s == n) return u;
+        if (tila[s] == 1) return 0;
+        tila[s] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (paino[s][i] > 0) {
+                int h = haku(i,Math.min(u,paino[s][i]));
+                if (h > 0) {
+                    paino[s][i] -= h;
+                    paino[i][s] += h;
+                    return h;
+                }
+            }
+        }
+        return 0;
+    }
     
     public int laske() {
-        
-        return getMaxFlow();
-    }
-    public List<List<Edge>> getGraph() {
-    solve();
-    return graph;
-  }
-
-  // Returns the maximum flow from the source to the sink.
-  public int getMaxFlow() {
-    solve();
-    return maxFlow;
-  }
-
-  // Returns the min-cut of this flow network in which the nodes on the "left side"
-  // of the cut with the source are marked as true and those on the "right side" 
-  // of the cut with the sink are marked as false.
-  public boolean[] getMinCut() {
-    solve();
-    return minCut;
-  }
-
-  // Performs the Ford-Fulkerson method applying a depth first search as
-  // a means of finding an augmenting path. The input consists of a directed graph
-  // with specified capacities on the edges.
-  public void solve() {
-    if (solved) return;
-
-    maxFlow = 0;
-    visited = new int[n];
-    minCut = new boolean[n];
-
-    // Find max flow.
-    int flow;
-    do {
-      // Try to find an augmenting path from source to sink
-      flow = dfs(source, Integer.MAX_VALUE);
-      visitedToken++;
-      maxFlow += flow;
-    } while(flow != 0);
-
-    // Find min cut.
-    for(int i = 0; i < n; i++)
-      if (visited[i] == visitedToken-1)
-        minCut[i] = true;
-
-    solved = true;
-  }
-
-  private int dfs(int node, int flow) {
-    // At sink node, return augmented path flow.
-    if (node == sink) return flow;
-
-    List<Edge> edges = graph.get(node);
-    visited[node] = visitedToken;
-
-    for (Edge edge : edges) {
-      if (visited[edge.to] != visitedToken && edge.capacity > 0) {
-
-        // Update flow to be bottleneck 
-        if (edge.capacity < flow) flow = edge.capacity;
-        int dfsFlow = dfs(edge.to, flow);
-
-        // Update edge capacities
-        if (dfsFlow > 0) {
-          Edge res = edge.residual;
-          edge.capacity -= dfsFlow;
-          res.capacity  += dfsFlow;
-          return dfsFlow;
+        while (true) {
+            tila = new int[n+1];
+            int u = haku(1,1<<20);
+            if (u == 0) break;
+            tulos += u;
         }
-
-      }
-    }
-    return 0;
-  }
-
-  // Construct an empty graph with n nodes including the source and sink nodes.
-  private void initializeGraph() {
-    graph = new ArrayList<>(n);
-    for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
-  }
-    
+        return tulos;
+    }    
 }
+ 
